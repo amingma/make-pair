097e3e7be3fecef40f4f82370ed4f40965a61717
amingma <jiama19999@gmail.com>
2024-02-27 00:38:12 -0600

    generateOrder complete

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
 using namespace std;
 vector<string> readNames(string fileName) {
     ifstream ifs(fileName);
@@ -26,9 +27,53 @@ vector<string> readNames() {
     while(cont == "Y" || cont == "y");
     return names;
 }
+void shuffle(vector<string>& names) {
+    int n = names.size();
+    for (int i=0; i<n; i++) {
+        int t = rand()%n;
+        string tmp = names[t];
+        names[t] = names[i];
+        names[i] = tmp;
+    }
+}
+void generateOrder() {
+    vector<string> names = readNames();
+    shuffle(names);
+    if (names.size()%2==1) {
+        names.push_back("Bye");
+    }
+    map<string, int> index;
+    int n = names.size();
+    for (int i=0; i<n; i++) {
+        index[names.at(i)] = i;
+    }
+    vector<vector<string>> pairs(n);
+    for (int i=0; i<n-1; i++) {
+        string tmp = names[n-1];
+        for (int j=n-1; j>=1; j--) {
+            if (j!=1) {
+                names[j] = names[j-1];
+            }
+            else {
+                names[j] = tmp;
+            }
+        }
+        for (int j=0; j<n; j++) {
+            pairs[index[names.at(j)]].push_back(names.at(n-1-j));
+        }
+    }
+    for (int i=0; i<n; i++) {
+        if (names.at(i)=="Bye") {
+            continue;
+        }
+        cout << names.at(i) << ": ";
+        for (string s: pairs[index[names.at(i)]]) {
+            cout << s << " ";
+        }
+        cout << "\n";
+    }
+}
 int main() {
-    ofstream ofs("./schedule.txt");
-    vector<string> test = readNames();
-    
+    generateOrder();
     return 0;
 }
